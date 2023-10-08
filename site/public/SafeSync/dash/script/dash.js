19a16bad8fab0a5c73869b08fbe97a572222fe3d
GustavoGomesDaSilva <gustavo.gomes.lhp@hotmail.com>
2023-10-08 01:56:11 -0300

    notificacoes e usuarios feitas, e tambem, consertado o front do login

--- site/public/SafeSync/dash/script/dash.js
+++ site/public/SafeSync/dash/script/dash.js
@@ -18,11 +18,15 @@ const areaTarefas = document.querySelector('#areaTarefas');
 function mostrarGraficos() {
     areaGraficos.style.display = 'block';
     areaTarefas.style.display = 'none';
+    tarefasLink.classList.remove('ativo');
+    dashboardLink.classList.toggle('ativo');
 }
 
 function mostrarTarefas() {
     areaGraficos.style.display = 'none';
     areaTarefas.style.display = 'block';
+    tarefasLink.classList.toggle('ativo');
+    dashboardLink.classList.remove('ativo');
 }
 
 // Chama as funções para inicializar o estado
@@ -31,3 +35,50 @@ mostrarGraficos();
 // Adiciona event listeners
 dashboardLink.addEventListener('click', mostrarGraficos);
 tarefasLink.addEventListener('click', mostrarTarefas);
+document.addEventListener('DOMContentLoaded', mostrarGraficos);
+
+
+// area modal notificacao
+
+// Referência ao modal e à div de notificação
+    document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.querySelector(".notificacaoModal");
+    var notificacao = document.querySelector("#notificacao");
+    var closeBtn = document.querySelector(".closeNotificacao");
+
+    notificacao.addEventListener("click", function() {
+        modal.style.display = "block";
+    });
+
+    closeBtn.addEventListener("click", function() {
+        modal.style.display = "none";
+    });
+
+    window.addEventListener("click", function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+});
+
+
+// area modal usuario
+
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.querySelector(".usuarioModal");
+    var usuario = document.querySelector("#usuario");
+    var closeBtn = document.querySelector(".closeUsuario");
+
+    usuario.addEventListener("click", function() {
+        modal.style.display = "block";
+    });
+
+ 
+
+    window.addEventListener("click", function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+});
+
